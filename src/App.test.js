80832739e37e649ae113d5b520a541099a208a4a
kolserdav <serega12101983@gmail.com>
2021-10-13 08:54:31 +0700

    Added private license

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,12 @@
+/******************************************************************************************
+ * Repository: https://github.com/kolserdav/api-prisma
+ * Author: Sergey Kolmiller
+ * Email: <uyem.ru@gmail.com>
+ * License: Proprietary and confidential
+ * License Text: Unauthorized copying of this file, via any medium is strictly prohibited
+ * Copyright: kolserdav (c), All rights reserved
+ * Create date: Wed Oct 13 2021 08:53:38 GMT+0700 (Krasnoyarsk Standard Time)
+******************************************************************************************/
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
